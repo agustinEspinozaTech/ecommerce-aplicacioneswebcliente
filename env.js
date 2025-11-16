1c4b701e939216b8e0dab68d9ec8a86e6ca8517e
agustin.espinoza <agustin.antonio.espinoza@istea.com.ar>
2025-11-16 17:50:25 -0300

    Agregar env.js con configuración de Airtable

---
+++ env.js
@@ -0,0 +1,5 @@
+export const AIRTABLE_TOKEN = "patFULm1O0uSeF0ik.cb47ea94a4f7bbaff58cb02161012860cbd8db409d7d9efb2c425dca28406571";
+export const BASE_ID = "app7iOzNOwDzNItIr";
+export const TABLE_NAME = "productos";
+export const CONTACT_TABLE_NAME = 'tblBpVBhCs3HumZyY'
+
